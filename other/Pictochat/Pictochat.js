4bd90e9620ed14e53db3fb8395582ff58ee6f139
Tycho10101 <supallawma@gmail.com>
2024-07-06 11:38:20 -1000

    Be able to refresh and code
    BRO MY INTERNET WENT OUT

--- other/Pictochat/Pictochat.js
+++ other/Pictochat/Pictochat.js
@@ -1,9 +1,32 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("Chat").innerHTML =
-    this.responseText;
-  }
-};
-xhttp.open("GET", "https://pictochat-web.glitch.me/data/chat.txt", true);
-xhttp.send();
+setInterval(GetChat, 3000);
+GetChat()
+var usernameVar;
+
+function GetChat() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			document.getElementById("Chat").innerHTML =
+			this.responseText;
+		}
+	};
+	xhttp.open("GET", "https://pictochat-web.glitch.me/data/chat.txt", true);
+	xhttp.send();
+} 
+
+function username() {
+	usernameVar = document.getElementById("Text").value;
+	document.getElementById("input").innerHTML = usernameVar+':<input type="text" id="Text" placeholder="Message"><button onclick="message()">Send</button>';
+}
+
+function message() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			LastMessage =
+			this.responseText;
+		}
+	};
+	xhttp.open("GET", "https://pictochat-web.glitch.me/send/"+usernameVar+"/"+document.getElementById("Text").value, true);
+	xhttp.send();
+}
